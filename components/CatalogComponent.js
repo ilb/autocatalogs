11f1db3463d90b968a2e97a8de7706939768bfff
dmitryi698 <dima2prog@gmail.com>
2022-10-13 17:08:22 +0400

    Fix: -Add catalogComponent example

---
+++ components/CatalogComponent.js
@@ -0,0 +1,13 @@
+import { Segment } from 'semantic-ui-react';
+
+const CatalogComponent = () => {
+	return (
+		<Segment>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias facilis, tempora molestiae
+			ipsum labore voluptate esse officiis aut quos error eius dolores, quas quod similique ipsa
+			suscipit. Inventore, ad esse?
+		</Segment>
+	);
+};
+
+export default CatalogComponent;
